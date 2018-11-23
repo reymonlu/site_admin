ac19b0d53f4db7d350cff25050751bb39711a285
Lucas <reymond.lu@gmail.com>
2018-11-23 16:44:05 +0100

    AJout de la class de vérification

--- controller/Identification.php
+++ controller/Identification.php
@@ -5,8 +5,23 @@
  * Date: 23/11/18
  * Time: 15:56
  */
+require_once("modele/Vue.php");
 
 class Identification
 {
+    public function afficheForm(){
+        # Création de la vue
+        $vue = new Vue();
+        $vue->show("./vue/Identification_form.html");
+    }
 
+    /**
+     * Fonction qui contrôle que tous les champs sont corrects
+     */
+
+    public function ControleForm(){
+        # Création de la vue
+
+
+    }
 }
