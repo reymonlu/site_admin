e18e13c52b0589603b547d15d81517fb89899f7b
evan <evan@evan.ovh>
2019-01-17 09:56:18 +0000

    ajout d'un formulaire bugé

---
+++ vue/start-stop-service.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site d'administration</title>
+</head>
+<body>
+<select name="cars">
+    <option value="start">start</option>
+    <option value="stop">stop</option>
+</select>
+
+<!--
+<select>
+<?php foreach ($this->list_service as $key => $value) { ?>
+    <option value="<?=$key?>"> <?=$key?></option>
+
+<?php } ?>
+</select>
+-->
+</body>
+</html>
+
